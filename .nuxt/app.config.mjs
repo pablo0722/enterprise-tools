2c0ea8e72b3347d755b20e48d65d681c0f0f23d5
Pablo Rodrigo D'angelo <pablo.dangelo@famar.com>
2022-12-05 00:18:18 -0300

    nuxtjs starting example

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/rdangelo/Documentos/aosps/Programas/enterprise-tools/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
